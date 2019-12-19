879f0697e4863bff783a84ebc49bb668bbbedcb6
daanishrasheed <daanishrasheed@gmail.com>
2019-12-19 15:20:57 -0500

    stuff added

---
+++ TWITOFF/__init__.py
@@ -0,0 +1,6 @@
+"""Entry point for our twitoff flask app"""
+
+
+from .app import create_app
+
+APP = create_app()
---
+++ TWITOFF/app.py
@@ -0,0 +1,14 @@
+"""COde for app"""
+
+from flask import Flask
+
+#make our app factory
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    
+    def root():
+        return 'Welcome to Twitoff!!'
+    return app
