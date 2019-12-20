c07f5a0c6fb5f99a19ed98548742f292d642475e
daanishrasheed <daanishrasheed@gmail.com>
2019-12-20 01:54:53 -0500

    changed schema and added reset route

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -21,4 +21,10 @@ def create_app():
     def root():
         users=User.query.all()
         return render_template('base.html', title = 'Home', users=users)
+    
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title = 'Reset', users=[])
     return app
